12107a743256bf262356977f8a1e5b2a21e32489
Riverlilyrs <riverlilyrs@gmail.com>
2026-01-19 00:54:38 -0800

    Yey!
    Buttons are pressed

--- MainMenu/MainMenu.cs
+++ MainMenu/MainMenu.cs
@@ -2,12 +2,18 @@ namespace UI.MainMenu {
 	using Godot;
 
 	public partial class MainMenu : Control {
-		[Export] private Button StartButton = null!;
-		[Export] private Button ExitButton = null!;
-
+		[Export] private Button NewGame = null!;
+		[Export] private Button LoadGame = null!;
+		[Export] private Button Options = null!;
+		[Export] private Button Settings = null!;
+		[Export] private Button Exit = null!;
+		
 		public override void _Ready() {
-			StartButton.Pressed += () => GD.Print("Start button pressed");
-			ExitButton.Pressed += () => GetTree().Quit();
-		}
+			NewGame.Pressed += () => GD.Print("Start button pressed");
+			LoadGame.Pressed += () => GD.Print("Load button pressed");
+			Options.Pressed += () => GD.Print("Options button pressed");
+			Settings.Pressed += () => GD.Print("Settings button pressed");
+			Exit.Pressed += () => GetTree().Quit();
+		}	
 	}
 }
