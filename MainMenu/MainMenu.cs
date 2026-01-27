33a5937e411fb5a802ecdfbb350a2707c53aed82
Kevin Munson <kitick538@gmail.com>
2026-01-26 23:16:33 -0800

    added functionality for scene buttons

--- MainMenu/MainMenu.cs
+++ MainMenu/MainMenu.cs
@@ -2,18 +2,24 @@ namespace UI.MainMenu {
 	using Godot;
 
 	public partial class MainMenu : Control {
+		[ExportCategory("Scenes")]
+		[Export] private PackedScene NewGameScene = null!;
+		[Export] private PackedScene LoadGameScene = null!;
+		[Export] private PackedScene SettingsScene = null!;
+
+		[ExportCategory("Buttons")]
 		[Export] private Button NewGame = null!;
 		[Export] private Button LoadGame = null!;
 		[Export] private Button Options = null!;
 		[Export] private Button Settings = null!;
 		[Export] private Button Exit = null!;
-		
+
 		public override void _Ready() {
-			NewGame.Pressed += () => GD.Print("Start button pressed");
-			LoadGame.Pressed += () => GD.Print("Load button pressed");
-			Options.Pressed += () => GD.Print("Options button pressed");
-			Settings.Pressed += () => GD.Print("Settings button pressed");
+			NewGame.Pressed += () => GetTree().ChangeSceneToPacked(NewGameScene);
+			LoadGame.Pressed += () => GetTree().ChangeSceneToPacked(LoadGameScene);
+			Options.Pressed += () => GetTree().ChangeSceneToPacked(SettingsScene);
+			Settings.Pressed += () => GetTree().ChangeSceneToPacked(SettingsScene);
 			Exit.Pressed += () => GetTree().Quit();
-		}	
+		}
 	}
 }
--- NewGame/NewGame.cs
+++ NewGame/NewGame.cs
@@ -2,14 +2,18 @@ namespace UI.NewGame {
 	using Godot;
 
 	public partial class NewGame : Control {
-		[Export] private Button StartButton = null!;
-		[Export] private Button ExitButton = null!;
-		
+		[ExportCategory("Scenes")]
+		[Export] private PackedScene NewGameScene = null!;
+		[Export] private PackedScene MainMenuScene = null!;
+
+		[ExportCategory("Buttons")]
 		[Export] private LineEdit FileName = null!;
+		[Export] private Button StartButton = null!;
+		[Export] private Button BackButton = null!;
 
 		public override void _Ready() {
-			StartButton.Pressed += () => GD.Print("Start button pressed");
-			ExitButton.Pressed += () => GetTree().Quit();
+			StartButton.Pressed += () => GetTree().ChangeSceneToPacked(NewGameScene);
+			BackButton.Pressed += () => GetTree().ChangeSceneToPacked(MainMenuScene);
 		}
 	}
 }
