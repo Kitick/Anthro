c5e227f3a046175f4048e2ada6e581ec4b120b31
Riverlilyrs <riverlilyrs@gmail.com>
2026-08-05 19:52:53 -0700

    whatever that just was

--- Lesson/HelloWorld.cs
+++ Lesson/HelloWorld.cs
@@ -1,1 +1,42 @@
-Console.WriteLine("Hello, world!");
+double x = 5.5;
+double y = 2.2;
+Console.WriteLine($"vector 1 = {x},{y}");
+
+double a = 5;
+double b = 4;
+
+Console.WriteLine($"vector 2 = {a},{b}");
+
+double idiot = x + a;
+double fuk = y + b;
+
+Console.WriteLine($"fucking idiot= {idiot},{fuk}");
+
+double T = 7;
+
+Console.WriteLine(T);
+Console.WriteLine("T");
+
+(double, double) fucking_idiot= (idiot,fuk);
+
+Console.WriteLine(fucking_idiot);
+
+double Jeff = (idiot);
+
+Console.WriteLine(Jeff);
+
+(double x, double y) vector1 = (5.5, 2.2);
+
+(double x, double y) vector2 = (5, 4);
+
+Console.WriteLine($"fucking idiot = {vector1},{vector2}");
+
+(double x, double y) vector3 = (vector1.x+vector2.x,vector1.y+vector2.y);
+
+Console.WriteLine(vector3);
+
+double dotproductshit = vector1.x * vector2.x + vector1.y * vector2.y;
+
+Console.WriteLine($"dotproductforKevin is the most glorious number that I will continue to talk about until the end of all time {dotproductshit}");
+
+Console.WriteLine($"Lesson 1 is done motherfuckers!!!(p.s. take that coding things)");
